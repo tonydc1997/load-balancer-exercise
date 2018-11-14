67aa2d74c547acee6d953ff050a580f05295ce03
Anthony Curtis <tonydc1997@gmail.com>
2018-11-14 11:31:04 -0800

    feat: Build out main feature of server var in server.js
    - Notify dev. that endpoint has been hit upon status 200
    
    - JSON.stringify message and net(work)

--- server.js
+++ server.js
@@ -10,4 +10,13 @@ const server = http.createServer(function (request, response) {
     console.log('favicon requested');
     return;
   }
+
+  response.writeHead(200, {
+    'Content-Type': 'application/json',
+  });
+  console.log('This endpoint has been hit');
+  response.end(JSON.stringify({
+    message: request.connection.remoteAddress,
+    net: os.networkInterfaces(),
+  }));
 });
