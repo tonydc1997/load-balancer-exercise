48ea76dac61ca8fb437280fd419030ba35cfca1e
Anthony Curtis <tonydc1997@gmail.com>
2018-11-14 11:27:22 -0800

    feat: Create server in server.js to prevents favicon errs from surfacing

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const os = require('os');
 const http = require('http');
 
+const server = http.createServer(function (request, response) {
+  if (request.url === '/favicon.ico') {
+    response.writeHead(200, {
+      'Content-Type': 'image/x-icon',
+    });
+    response.end();
+    console.log('favicon requested');
+    return;
+  }
+});
